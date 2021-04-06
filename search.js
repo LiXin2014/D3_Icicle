03a26be967d3e626aba0c8ddd31bdac872e551e0
Xin Li <LiXin2014@users.noreply.github.com>
2021-04-06 15:40:10 -0700

    Delete search.js
    Not used.

--- search.js
+++
@@ -1,9 +0,0 @@
-import rects from './script.js';
-
-const onSearch = () => {
-    let term = document.getElementById("term").value;
-    // d.data.name.indexOf(term) != 0;
-    rects.each(rect => {
-      let index = rect.data.name.indexOf(term)
-    })
-  }
